2a9e6e608bffb82dddfa4025f97d2ac6ba163cf2
Nina <gadgetgeekette@hotmail.co.uk>
2024-06-19 17:29:27 +0100

    Refactored article vote

--- src/components/ArticleVote.jsx
+++ src/components/ArticleVote.jsx
@@ -1,50 +1,39 @@
 import IconButton from '@mui/material/IconButton'
 import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
 import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import {updateArticleVote} from './api'
 
 // TODO: after user login implemented, prevent user voting multiple times across different sessions
 
 function ArticleVote({article}) {
     
-    const [votes, setVotes] = useState(article.voteCount);
-    const [incrVote, setIncrVote] = useState(0);
+    const [votesCount, setVotesCount] = useState(article.voteCount);
+    const [incrVote, setIncrVote] = useState(-1);
     const [errMessage, setErrMessage] = useState('');
 
-    useEffect(() => {
-        if (incrVote !== 0) {
-            updateArticleVote(article.id, incrVote)
-                .then((articleData) => {
-                    setVotes(articleData.votes);
-                })
-                .catch((err) => {
-                    const errMsg = err.msg
-                        ? err.msg
-                        : 'An unknown error occurred';
-                    setErrMessage(errMsg);
-                });
-        }
-    }, [article, incrVote]);
-
     function handleClick() {
-        const incrVoteVal = incrVote <= 0
+        const incrVoteVal = incrVote === -1
             ? 1
             : -1;
+        setVotesCount(article.voteCount + incrVoteVal) // optimistic rendering
         setIncrVote(incrVoteVal);
+        updateArticleVote(article.id, incrVoteVal)
+            .then((articleData) => {
+                setVotesCount(articleData.votes);
+            })
+            .catch((err) => {
+                const errMsg = err.msg
+                    ? err.msg
+                    : 'An unknown error occurred';
+                setErrMessage(errMsg);
+            });
     }
 
     function getIcon() {
         return incrVote <= 0
             ? (<ThumbUpOffAltIcon />)
             : (<ThumbUpAltIcon />);
-    }
-
-    function getVotes() {
-        if (votes) {
-            return votes;
-        }
-        return article.voteCount;
     }
 
     return errMessage
@@ -55,7 +44,7 @@ function ArticleVote({article}) {
         </div>)
 
         : (<div>
-            <div>Votes: {getVotes()}</div>
+            <div>Votes: {votesCount}</div>
             <IconButton size="small" onClick={handleClick}>
                 {getIcon()}
             </IconButton>
